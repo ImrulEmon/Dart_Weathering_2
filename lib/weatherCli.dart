1765dd0f4cdb7ebf7abf6892c16b59e7222929f9
ImrulEmon <emimrulemon@gmail.com>
2022-10-20 15:03:36 +0600

    works

--- lib/weatherCli.dart
+++ lib/weatherCli.dart
@@ -11,16 +11,36 @@ Future weatherCli(String? city) async {
 
       stdout.writeln('');
       stdout.writeln(
-          'Weather in ${apiResult.name} ${String.fromCharCodes(rainBow)}');
-      stdout.writeln('---------------------------------------');
+          'Weather in \u001b[32m ${apiResult.name},${apiResult.sys?.country} \u001b[0m${String.fromCharCodes(rainBow)}');
+      stdout.writeln('===========================================');
       final date = DateTime.fromMillisecondsSinceEpoch(
           apiResult.dt!.toInt() * 1000,
           isUtc: true);
-      final dateTimeString = '${date.day}.${date.month}.${date.year} :';
+      final dateTimeString =
+          '\u001b[35m Date : ${date.day}.${date.month}.${date.year}\u001b[0m';
       final tempString =
-          'Now : ${apiResult.main?.temp} ℃ , Feels like : ${apiResult.main?.feelsLike} ℃ , Min : ${apiResult.main?.tempMin} ℃ , Max : ${apiResult.main?.tempMax} ℃';
+          '\u001b[34m Now : ${apiResult.main?.temp} ℃ \u001b[0m,\u001b[36m Feels like : ${apiResult.main?.feelsLike} ℃ \u001b[0m,\u001b[32m Min : ${apiResult.main?.tempMin} ℃ \u001b[0m,\u001b[31m Max : ${apiResult.main?.tempMax} ℃ \u001b[0m';
+      final condition = '\u001b[35m ${apiResult.weather?[0].main} \u001b[0m';
       stdout.writeln(dateTimeString);
       stdout.writeln(tempString);
+      stdout.write(condition);
+
+      if (condition == 'Thunderstorm') {
+        print('${String.fromCharCodes(thunderStorm)}');
+      } else if (condition == 'Rain') {
+        print('${String.fromCharCodes(rain)}');
+      } else if (condition == 'Snow') {
+        print('${String.fromCharCodes(snow)}');
+      } else if (condition == 'Clouds') {
+        print('${String.fromCharCodes(clouds)}');
+      } else if (condition == 'Clear') {
+        print('${String.fromCharCodes(clear)}');
+      } else if (condition == 'Drizzle') {
+        print('${String.fromCharCodes(drizzle)}');
+      } else {
+        print('${String.fromCharCodes(others)}');
+      }
+
       stdout.writeln('');
     } catch (e) {
       stderr.writeln('error : networking error');
--- lib/weathering.dart
+++ lib/weathering.dart
@@ -3,7 +3,7 @@ import 'package:weathering/weatherCli.dart';
 
 void weathering() {
   String? city;
-  stdout.write('Enter city Name : ');
+  stdout.write('\u001b[34m Enter city Name : \u001b[0m');
   city = stdin.readLineSync();
 
   weatherCli(city);
